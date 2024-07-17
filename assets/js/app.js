06afc0033b64feac7941722bef7cf4a284abda73
Adhiraj Pawar <adhirajpawar3@gmail.com>
2024-07-17 23:09:40 +0530

    personal portfolio(frontend)

---
+++ assets/js/app.js
@@ -0,0 +1,162 @@
+// navigation bar  effects on scroll 
+window.addEventListener('scroll', function() {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+});
+
+// services section - modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var  modal = function(modalClick) {
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i)=> {
+    learnmoreBtn.addEventListener("click", () =>{
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) =>{
+    modalCloseBtn.addEventListener("click",() =>{
+        serviceModals.forEach((modalView) =>{
+            modalView.classList.remove("active");
+        });
+    });
+});
+
+// portfoilo section - modal
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioClosebtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick) {
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) =>{
+    imgCard.addEventListener("click",() => {
+        portfolioModal(i);
+    });
+
+});
+portfolioClosebtns.forEach((portfolioClosebtn) =>{
+    portfolioClosebtn.addEventListener("click",() =>{
+        portfolioModals.forEach((portfolioModalView) =>{
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+// our clients - swiper
+var swiper = new Swiper(".client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// website light/dark theme
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark-theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved-theme",getCurrentTheme());
+    localStorage.setItem("saved-icon",getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon =   localStorage.getItem("saved-icon");
+
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"] ("dark-theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"] ("sun");
+}
+
+
+
+
+// scroll to top botton
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function(){
+scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click",() => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+// navigation menu items active on page scroll
+window.addEventListener("scroll",() => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        if(scrollY > scrollTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+
+        }
+        else{
+            document.querySelector(".nav-items a[href*=" + id + "]").classList.add("remomve");
+        }
+    });
+});
+
+// responsive navigation menu toogle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click",() =>{
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click",() =>{
+    navigation.classList.remove("active");
+});
+
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click",()=>{
+        navigation.classList.remove("active");
+    });
+});
+
+
+// scroll reveal animations
+// common reveal options to create reveal animations
+ScrollReveal({
+     reset: true,
+     distance:'60px',
+     duration:2500,
+     delay:100  
+     });
+
+// target elements and specify options to create reveal animations
+ScrollReveal().reveal('.home .info h2',{delay:500, origin:'left'});
+ScrollReveal().reveal('.home .info h3, .home .info p',{delay:600, origin:'right'});
+ScrollReveal().reveal('.home .info btn',{delay:700, origin:'bottom'});
+ScrollReveal().reveal('.media-icons i',{delay:500, origin:'left', interval:200});
+ScrollReveal().reveal('.home-img',{delay:500, origin:'bottom'});
